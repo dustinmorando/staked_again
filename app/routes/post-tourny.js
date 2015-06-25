73e34168fab6758d96c0f08516071d30d43e2b35
Dustin Morando <dustinmorando@me.com>
2015-06-25 14:18:31 -0400

    updated ember and ember data i think

--- app/routes/post-tourny.js
+++ app/routes/post-tourny.js
@@ -3,7 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(){
     return{
-      wsop: {}
+      wsop: [
+      {
+        
+      }
+      ]
     };
   }
 });
