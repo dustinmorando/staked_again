2496544e723e141032956f8e2271d10a8b426a9b
Dustin Morando <dustinmorando@me.com>
2015-06-26 12:14:16 -0400

    added api keys to config enviroment

--- config/environment.js
+++ config/environment.js
@@ -12,10 +12,19 @@ module.exports = function(environment) {
         // e.g. 'with-controller': true
       }
     },
+    contentSecurityPolicy: {
+        'default-src': "'none'",
+        'script-src': "'self' 'unsafe-inline' 'unsafe-eval' use.typekit.net connect.facebook.net maps.googleapis.com maps.gstatic.com",
+        'font-src': "'self' data: use.typekit.net",
+        'connect-src': "'self'",
+        'img-src': "'self' www.facebook.com p.typekit.net",
+        'style-src': "'self' 'unsafe-inline' use.typekit.net",
+        'frame-src': "s-static.ak.facebook.com static.ak.facebook.com www.facebook.com"
+      },
 
     APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
+      applicationId: 'tKJS4s192gVimFWvf0ue8HWYux3kqjpqFn8tqNSg',
+      restApiId: 'VsLJiNi2fS6vCtx9jUeJOVMsrEXHZUCxk7oFKeSL'
     }
   };
 
